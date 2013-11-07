c27fc130bc9dd0af563ceca8d90c564d1cd3ac94
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-11-07 18:34:02 +0100

    fix release script

--- mk-release.sh
+++ mk-release.sh
@@ -5,7 +5,7 @@ echo "usage: mk-release [VERSION]"
 VERSION="$1"
 echo "archiving for version $VERSION"
 
-EXCLUDES="--exclude=.git --exclude=_build --exclude=man --exclude=doc --exclude=.*.sw*"
+EXCLUDES="--exclude=.git --exclude=_build --exclude=man --exclude=doc --exclude=.*.sw* --exclude setup.data --exclude setup.log"
 OUTPUT="qcheck-$VERSION".tar.gz
 PREFIX="--transform s+^\./+qcheck-$VERSION/+"
 
