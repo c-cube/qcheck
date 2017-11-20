3220873e0c744a07106a118a50c28c52b9b4bb71
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-11-20 11:39:44 +0100

    update copyright header (closes #47)

--- src/QCheck.ml
+++ src/QCheck.ml
@@ -1,20 +1,8 @@
 
 (*
 QCheck: Random testing for OCaml
-Copyright (C) 2016  Vincent Hugot, Simon Cruanes
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Library General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Library General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+copyright (c) 2013-2017, Guillaume Bury, Simon Cruanes, Vincent Hugot, Jan Midtgaard
+all rights reserved.
 *)
 
 (** {1 Quickcheck inspired property-based testing} *)
