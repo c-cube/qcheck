d5b9877739f0d24fb75c4300f125456f762df502
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-09-17 22:17:56 -0500

    doc: improve formatting and add index file for `qcheck`

--- src/alcotest/QCheck_alcotest.mli
+++ src/alcotest/QCheck_alcotest.mli
@@ -4,9 +4,9 @@
     We use environment variables for controlling QCheck here, since alcotest
     doesn't seem to provide a lot of flexibility.
 
-    [QCHECK_VERBOSE] if "1" or "true", will make tests verbose
-    [QCHECK_SEED] if an integer, will fix the seed
-    [QCHECK_LONG] is present, will trigger long tests
+    - [QCHECK_VERBOSE] if "1" or "true", will make tests verbose
+    - [QCHECK_SEED] if an integer, will fix the seed
+    - [QCHECK_LONG] is present, will trigger long tests
 
     @since 0.9
 *)
