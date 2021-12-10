961928730fe3e78b8d88a34807690defd656482a
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-12-10 14:19:56 +0100

    Use environement variable instead of impure ref

--- src/ppx_deriving_qcheck/ppx_deriving_qcheck.ml
+++ src/ppx_deriving_qcheck/ppx_deriving_qcheck.ml
@@ -4,8 +4,8 @@ module O = G.Observable
 
 (** {1. ppx_deriving_qcheck} *)
 
-(** ppx_deriving_qcheck is a ppx deriver for QCheck generators. It does a traversal
-    map on type declarations annoted with [QCheck].
+(** ppx_deriving_qcheck is a ppx deriver for QCheck generators. It does a
+    traversal map on type declarations annoted with [QCheck].
 
     Example:
     {[
@@ -39,7 +39,7 @@ let pat ~loc ?sized s =
 (** Recursive generators must be treated separatly:
 
     {[
-    type 'a list = Cons of 'a * 'a list | Nil 
+    type 'a list = Cons of 'a * 'a list | Nil
     ]}
 
     becomes:
@@ -53,9 +53,23 @@ let pat ~loc ?sized s =
     let gen_list_sized gen_a = sized @@ (gen_list_sized gen_a)
     ]}
 
-    In the basic derivation {[ 'a list ]} would be translated to {[gen_list]}. However,
-    we want the generator to call itsef.
+    In the basic derivation {[ 'a list ]} would be translated to {[gen_list]}.
+    However, we want the generator to call itsef.
  *)
+
+module Env = struct
+  (** [env] contains:
+      - the list of recursive types during the derivation
+      - the list of types to derive (i.e. mutual types)
+      - the current type to derive *)
+  type env = {
+      rec_types : string list;
+      curr_types : string list;
+      curr_type : string;
+    }
+
+  let is_rec env x = List.mem x env.rec_types
+end
 
 let rec longident_to_str = function
   | Lident s -> s
@@ -63,67 +77,40 @@ let rec longident_to_str = function
   | Lapply (lg1, lg2) ->
       Printf.sprintf "%s %s" (longident_to_str lg1) (longident_to_str lg2)
 
-let rec is_rec_typ typ_names = function
+let rec is_rec_typ env = function
   | { ptyp_desc = Ptyp_constr ({ txt = x; _ }, _); _ } ->
-     List.exists (fun typ_name -> longident_to_str x = typ_name) typ_names
-  | { ptyp_desc = Ptyp_tuple xs; _ } -> List.exists (is_rec_typ typ_names) xs
+     List.exists (fun typ_name -> longident_to_str x = typ_name) env.Env.curr_types
+  | { ptyp_desc = Ptyp_tuple xs; _ } -> List.exists (is_rec_typ env) xs
   | { ptyp_desc = Ptyp_variant (rws, _, _); _ } ->
-     List.exists (fun rw ->
-         match rw.prf_desc with
-         | Rtag (lab, _, cts) ->
-            List.exists (fun typ_name -> lab.txt = typ_name) typ_names ||
-              List.exists (is_rec_typ typ_names) cts
-         | Rinherit ct -> is_rec_typ typ_names ct) rws
+     List.exists (is_rec_row_field env) rws
   | _ -> false
 
-let is_rec_constr_decl typ_names cd =
+and is_rec_row_field env rw =
+  match rw.prf_desc with
+  | Rtag (lab, _, cts) ->
+     List.exists (fun typ_name -> lab.txt = typ_name) env.Env.curr_types ||
+       List.exists (is_rec_typ env) cts
+  | Rinherit ct -> is_rec_typ env ct
+
+let is_rec_constr_decl env cd =
   match cd.pcd_args with
-  | Pcstr_tuple cts -> List.exists (is_rec_typ typ_names) cts
+  | Pcstr_tuple cts -> List.exists (is_rec_typ env) cts
   | _ -> false
 
-(** [is_rec_type_decl typ_names typ] looks for elements of [typ_names]
+(** [is_rec_type_decl env typ] looks for elements of [env.curr_types]
     recursively in [typ]. *)
-let is_rec_type_decl typ_names typ =
+let is_rec_type_decl env typ =
   let in_type_kind =
     match typ.ptype_kind with
-    | Ptype_variant cstrs -> List.exists (is_rec_constr_decl typ_names) cstrs
+    | Ptype_variant cstrs -> List.exists (is_rec_constr_decl env) cstrs
     | _ -> false
   in
   let in_manifest =
     match typ.ptype_manifest with
-    | Some x -> is_rec_typ typ_names x
+    | Some x -> is_rec_typ env x
     | None -> false
   in
   in_type_kind || in_manifest
-
-(** [env] contains the list of recursive types during the derivation
-
-    i.e:
-    - contains one element maximum if its a single type declaration *)
-type env = string list ref
-
-(** [curr_types] saves both the current type and all mutual
-    recursive types *)
-let curr_types : (string * string list) option ref = ref None
-
-let set_curr_types x xs = curr_types := Some (x, xs)
-
-let clean_curr_types () = curr_types := None
-
-let curr_type () = Option.get !curr_types |> fst
-
-let curr_types () = Option.get !curr_types |> snd
-
-let env : env = ref []
-
-let clean_env () = env := []
-
-let is_rec x = List.mem x !env
-
-let add_env typ_names typ_name ty =
-  if is_rec_type_decl typ_names ty then
-    env := typ_name :: !env
-
 
 (** {2. Generator constructors} *)
 
@@ -133,11 +120,11 @@ let add_env typ_names typ_name ty =
     - Lident s: We transform to gen_s (or gen if s = "t")
     - Ldot (lg, s): We transform to qualified generator (e.g. B.gen)
 *)
-let gen_longident ~loc lg args =
+let gen_longident ~loc ~env lg args =
   let (module A) = Ast_builder.make loc in
   match lg with
   | Lident s ->
-     if is_rec s then
+     if Env.is_rec env s then
        name ~sized:true s |> A.evar |>
          Args.apply_args ~loc args |>
          Args.apply_args ~loc [ [%expr (n / 2)] ]
@@ -260,7 +247,7 @@ let gen_record ~loc ~gens ?(f = fun x -> x) xs =
 (** {2. Core derivation} *)
 
 (** [gen_from_type typ] performs the AST traversal and derivation to qcheck generators *)
-let rec gen_from_type ~loc typ =
+let rec gen_from_type ~loc ~env typ =
   Option.value (Attributes.gen typ)
     ~default:
       (match typ with
@@ -272,25 +259,25 @@ let rec gen_from_type ~loc typ =
       | [%type: float] -> G.float loc
       | [%type: int32] | [%type: Int32.t] -> G.int32 loc
       | [%type: int64] | [%type: Int64.t] -> G.int64 loc
-      | [%type: [%t? typ] option] -> G.option ~loc (gen_from_type ~loc typ)
-      | [%type: [%t? typ] list] -> G.list ~loc (gen_from_type ~loc typ)
-      | [%type: [%t? typ] array] -> G.array ~loc (gen_from_type ~loc typ)
+      | [%type: [%t? typ] option] -> G.option ~loc (gen_from_type ~loc ~env typ)
+      | [%type: [%t? typ] list] -> G.list ~loc (gen_from_type ~loc ~env typ)
+      | [%type: [%t? typ] array] -> G.array ~loc (gen_from_type ~loc ~env typ)
       | { ptyp_desc = Ptyp_tuple typs; _ } ->
-          let tys = List.map (gen_from_type ~loc) typs in
+          let tys = List.map (gen_from_type ~loc ~env) typs in
           gen_tuple ~loc tys
       | { ptyp_desc = Ptyp_constr ({ txt = ty; _ }, args); _ } ->
-         let args = List.map (gen_from_type ~loc) args in
-         gen_longident ~loc ty args
-      | { ptyp_desc = Ptyp_var s; _ } -> gen_longident ~loc (Lident s) []
+         let args = List.map (gen_from_type ~loc ~env) args in
+         gen_longident ~loc ~env ty args
+      | { ptyp_desc = Ptyp_var s; _ } -> gen_longident ~loc ~env (Lident s) []
       | { ptyp_desc = Ptyp_variant (rws, _, _); _ } ->
-          gen_from_variant ~loc rws
+          gen_from_variant ~loc ~env rws
       | { ptyp_desc = Ptyp_arrow (_, left, right); _ } ->
-          gen_from_arrow ~loc  left right
+          gen_from_arrow ~loc ~env left right
       | _ ->
           Ppxlib.Location.raise_errorf ~loc
             "This type is not supported in ppx_deriving_qcheck")
 
-and gen_from_constr ~loc { pcd_name; pcd_args; pcd_attributes; _ } =
+and gen_from_constr ~loc ~env { pcd_name; pcd_args; pcd_attributes; _ } =
   let (module A) = Ast_builder.make loc in
   let constr_decl =
     A.constructor_declaration ~name:pcd_name ~args:pcd_args ~res:None
@@ -302,43 +289,39 @@ and gen_from_constr ~loc { pcd_name; pcd_args; pcd_attributes; _ } =
     | Pcstr_tuple [] | Pcstr_record [] ->
         G.pure ~loc @@ A.econstruct constr_decl None
     | Pcstr_tuple xs ->
-        let tys = List.map (gen_from_type ~loc ) xs in
+        let tys = List.map (gen_from_type ~loc ~env) xs in
         gen_tuple ~loc ~f:mk_constr tys
     | Pcstr_record xs ->
-        let tys = List.map (fun x -> gen_from_type ~loc  x.pld_type) xs in
+        let tys = List.map (fun x -> gen_from_type ~loc ~env x.pld_type) xs in
         gen_record ~loc ~f:mk_constr ~gens:tys xs
   in
 
   A.pexp_tuple [ Option.value ~default:[%expr 1] weight; gen ]
 
-and gen_from_variant ~loc rws =
+and gen_from_variant ~loc ~env rws =
   let (module A) = Ast_builder.make loc in
-  let is_rec (row : row_field) : bool =
-    match row.prf_desc with
-    | Rinherit _ -> false
-    | Rtag (_, _, typs) -> List.exists (is_rec_typ (curr_types ())) typs
-  in
+  let is_rec = is_rec_row_field env in
   let to_gen (row : row_field) : expression =
     let w =
       Attributes.weight row.prf_attributes |> Option.value ~default:[%expr 1]
     in
     let gen =
       match row.prf_desc with
-      | Rinherit typ -> gen_from_type ~loc typ
+      | Rinherit typ -> gen_from_type ~loc ~env typ
       | Rtag (label, _, []) -> G.pure ~loc @@ A.pexp_variant label.txt None
       | Rtag (label, _, typs) ->
           let f expr = A.pexp_variant label.txt (Some expr) in
-          gen_tuple ~loc ~f (List.map (gen_from_type ~loc) typs)
+          gen_tuple ~loc ~f (List.map (gen_from_type ~loc ~env) typs)
     in
     [%expr [%e w], [%e gen]]
   in
   let gen = gen_sized ~loc is_rec to_gen rws in
-  let typ_t = A.ptyp_constr (A.Located.mk @@ Lident (curr_type ())) [] in
+  let typ_t = A.ptyp_constr (A.Located.mk @@ Lident env.curr_type) [] in
   let typ_gen = A.Located.mk @@ Lident G.ty in
   let typ = A.ptyp_constr typ_gen [ typ_t ] in
   [%expr ([%e gen] : [%t typ])]
 
-and gen_from_arrow ~loc left right =
+and gen_from_arrow ~loc ~env left right =
   let rec observable = function
     | [%type: unit] -> O.unit loc
     | [%type: bool] -> O.bool loc
@@ -361,7 +344,7 @@ and gen_from_arrow ~loc left right =
         let res, xs = aux xs in
         let obs = observable x in
         (res, [%expr [%e obs] @-> [%e xs]])
-    | x -> (gen_from_type ~loc  x, [%expr o_nil])
+    | x -> (gen_from_type ~loc ~env x, [%expr o_nil])
   in
   let x, obs = aux right in
   (* TODO: export this in qcheck_generators for https://github.com/c-cube/qcheck/issues/190 *)
@@ -382,10 +365,10 @@ and gen_from_arrow ~loc left right =
     the sized generator version, and a normal generator using this last with
     [Gen.sized].
 *)
-let gen_from_type_declaration ~loc td =
+let gen_from_type_declaration ~loc ~env td =
   let (module A) = Ast_builder.make loc in
-  let ty = curr_type () in
-  let is_rec = is_rec (curr_type ()) in
+  let ty = env.Env.curr_type in
+  let is_rec = Env.is_rec env ty in
 
   let args =
     List.map
@@ -400,14 +383,14 @@ let gen_from_type_declaration ~loc td =
   let gen =
     match td.ptype_kind with
     | Ptype_variant xs ->
-       let is_rec cd = is_rec_constr_decl (curr_types ()) cd in
-       gen_sized ~loc is_rec (gen_from_constr ~loc) xs
+       let is_rec cd = is_rec_constr_decl env cd in
+       gen_sized ~loc is_rec (gen_from_constr ~loc ~env) xs
     | Ptype_record xs ->
-        let gens = List.map (fun x -> gen_from_type ~loc  x.pld_type) xs in
+        let gens = List.map (fun x -> gen_from_type ~loc ~env x.pld_type) xs in
         gen_record ~loc ~gens xs
     | _ ->
         let typ = Option.get td.ptype_manifest in
-        gen_from_type ~loc  typ
+        gen_from_type ~loc ~env typ
   in
 
   let pat_gen = pat ~loc ty in
@@ -427,7 +410,6 @@ let gen_from_type_declaration ~loc td =
         [%stri let [%p pat_gen] = [%e gen_normal]]
       )
 
-(** *)
 let mutually_recursive_gens ~loc gens =
   let (module A) = Ast_builder.make loc in
   let to_mutualize_gens =
@@ -450,37 +432,36 @@ let mutually_recursive_gens ~loc gens =
   let mutual_gens = A.pstr_value Recursive gens in
   mutual_gens :: normal_gens
 
-(** [derive_gen ~loc xs] creates generators for type declaration in [xs].
-
-    It also has a hidden purpose: it sets the environment prior to
-    the derivation. It identifies recursive types declarations (e.g. list)
-    and its external arguments (e.g. 'a list).
-*)
+(** [derive_gen ~loc xs] creates generators for type declaration in [xs]. *)
 let derive_gen ~loc (xs : rec_flag * type_declaration list) : structure =
-  (match xs with
+  let open Env in
+  let add_if_rec env typ x =
+    if is_rec_type_decl env typ then
+      { env with rec_types = x :: env.rec_types}
+    else env
+  in
+  match xs with
   | (_, [ x ]) ->
-     let () = set_curr_types x.ptype_name.txt [x.ptype_name.txt] in
-     let () = add_env [x.ptype_name.txt] x.ptype_name.txt x in
-     (match gen_from_type_declaration ~loc x with
-     | `Recursive (gen_sized, gen) -> [gen_sized; gen]
-     | `Normal gen -> [gen])
+     let typ_name = x.ptype_name.txt in
+     let env = { curr_type = typ_name; rec_types = []; curr_types = [typ_name] } in
+     let env = add_if_rec env x typ_name in
+     (match gen_from_type_declaration ~loc ~env x with
+      | `Recursive (gen_sized, gen) -> [gen_sized; gen]
+      | `Normal gen -> [gen])
   | _, xs ->
      let typ_names = List.map (fun x -> x.ptype_name.txt) xs in
-     let () =
-       List.map (fun x -> (x.ptype_name.txt, x)) xs |>
-         List.iter (fun (x, y) -> add_env typ_names x y)
+     let env = { curr_type = ""; rec_types = []; curr_types = typ_names } in
+     let env =
+       List.fold_left
+         (fun env x -> add_if_rec env x x.ptype_name.txt)
+         env xs
      in
      let gens =
        List.map (fun x ->
-           let () = set_curr_types x.ptype_name.txt typ_names in
-           gen_from_type_declaration ~loc x) xs
+           let env = { env with curr_type = x.ptype_name.txt }in
+           gen_from_type_declaration ~loc ~env x) xs
      in
-     mutually_recursive_gens ~loc gens)
-  |>
-    fun res ->
-    let () = clean_env () in
-    let () = clean_curr_types () in
-    res
+     mutually_recursive_gens ~loc gens
 
 (** {2. Ppxlib machinery} *)
 
