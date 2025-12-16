f69725fa02969ff9191058f4437a5ce123547156
Jan Midtgaard <mail@janmidtgaard.dk>
2025-12-16 13:06:42 +0100

    Revert "Add a QCheck.opt alias for consistency"
    This reverts commit 770bb1c100d54eaf3db94dc4b535099bf985fbf0.

--- src/core/QCheck.mli
+++ src/core/QCheck.mli
@@ -1580,10 +1580,6 @@ val array_of_size : int Gen.t -> 'a arbitrary -> 'a array arbitrary
 val option : ?ratio:float -> 'a arbitrary -> 'a option arbitrary
 (** Choose between returning Some random value with optional ratio, or None. *)
 
-val opt : ?ratio:float -> 'a arbitrary -> 'a option arbitrary
-(** Synonym for {!option}.
-    @since NEXT_RELEASE *)
-
 val result : ?ratio:float -> 'a arbitrary -> 'e arbitrary -> ('a, 'e) result arbitrary
 (** [result ~ratio okgen errgen] generates [Ok v] with [v] coming from [okgen]
     or [Error e] with [e] coming from [errgen], depending on [ratio]. The latter
